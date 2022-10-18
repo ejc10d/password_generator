34270834688a6a47a44eee1842783b27fd55dfc1
ejc10d <112822833+ejc10d@users.noreply.github.com>
2022-10-18 17:43:07 -0400

    added choice function and prompt window

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomChoice(list) {
+  return list[randomInt(list.length)]
+}
+
+function generatePassword() {
+
+ var userInput = window.prompt("How many characters would you like your password to be? Note: passwords must be 8-120 characters long.")
+
+ var passwordLength = parseInt (userInput)
+
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength >128) {
+  window.alert("Please enter a number between 8 and 128.")
+  return
+ }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

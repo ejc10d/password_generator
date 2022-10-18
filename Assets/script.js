434ef9cc97e7ac656943b16c00a3a97d700a2026
ejc10d <112822833+ejc10d@users.noreply.github.com>
2022-10-18 17:46:16 -0400

    added options for each option list

--- Assets/script.js
+++ Assets/script.js
@@ -20,7 +20,11 @@ if (isNaN(passwordLength) || passwordLength < 8 || passwordLength >128) {
  var wantLower = window.confirm("Would you like your password to have lower-case letters?")
  var wantUpper = window.confirm("Would you like your password to have upper-case letters?")
 
- 
+ var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ var symbolList = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "~"]
+ var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ var upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
